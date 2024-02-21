df5dd36813bc5661912037f341f8852c5ff0c9c2
havrylivdenys <denys.havryliv@gmail.com>
2024-02-21 19:04:08 +0200

    fix: linters added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
-const app = express();
-const port = 3000;
-const prisma = new PrismaClient();
+const app = express()
+const port = 3000
+const prisma = new PrismaClient()
 
-app.get("/", async (req, res) => {
-  const movies = await prisma.movies.findMany();
-  res.json(movies);
-});
+app.get('/', (req, res) => {
+  prisma.movies
+    .findMany()
+    .then((movies) => res.status(200).send(movies))
+    .catch((error) => res.status(500).send(error))
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
--- src/movies_data.ts
+++ src/movies_data.ts
@@ -1,25 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-async function main() {
+async function main (): Promise<void> {
   const user = await prisma.movies.create({
     data: {
-        Title: "Toy Story",
-        Director: "John Lasseter",
-        Year: 1995,
-        Length_minutes: 81
-    },
-  });
-  console.log(user);
+      Title: 'Toy Story',
+      Director: 'John Lasseter',
+      Year: 1995,
+      Length_minutes: 81
+    }
+  })
+  console.log(user)
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })

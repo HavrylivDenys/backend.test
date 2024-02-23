01420f56d285e689a9e0c02ab5a81c06e5597825
havrylivdenys <denys.havryliv@gmail.com>
2024-02-23 16:30:27 +0200

    feat: adding semantic release part-2

--- release.config.js
+++ release.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    branches: ['develop'],
-    repositoryUrl: 'https://github.com/vitalik203/Resume_seven.git',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      {
-        // Custom configuration for @semantic-release/git plugin
-        "assets": ["CHANGELOG.md"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      },
-      '@semantic-release/github',
-      '@semantic-release/npm',
-    ],
-  };
-  
+  branches: ['develop'],
+  repositoryUrl: 'https://github.com/vitalik203/Resume_seven.git',
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    '@semantic-release/changelog',
+    {
+      // Custom configuration for @semantic-release/git plugin
+      "assets": ["CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }
+  ],
+};

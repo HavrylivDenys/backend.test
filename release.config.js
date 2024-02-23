40e167e58032dbec9949d67b7985ad14abe5a015
HavrylivDenys <denys.havryliv@gmail.ocm>
2024-02-23 17:58:34 +0200

    feat: adding semantic release part-10

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: ['main'],
   debug: true,
   ci: true,
-  dryRun: true,
+  dryRun: false,
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",

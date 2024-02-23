870efc7e2730787af216a1cceb716151052241ee
HavrylivDenys <denys.havryliv@gmail.ocm>
2024-02-23 18:12:09 +0200

    feat: adding semantic release part-11

--- release.config.js
+++ release.config.js
@@ -5,17 +5,17 @@ module.exports = {
   dryRun: false,
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
     ['@semantic-release/changelog',
       {
         // Custom configuration for @semantic-release/git plugin
         "changelogFile": "CHANGELOG.md",
       }
     ],
+    "@semantic-release/release-notes-generator",
     ["@semantic-release/git", {
       "assets": ["CHANGELOG.MD", "package.json", "package-lock.json"],
       "message": "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
     }],
+    "@semantic-release/github",
   ],
 };
